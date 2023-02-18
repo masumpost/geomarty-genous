fc1268c8014c103cb5430c28acae3f30ae5e5fdb
md-masum <masum.nextu@gmail.com>
2023-02-18 23:25:40 +0600

    hover color added

--- js/felements.js
+++ js/felements.js
@@ -1,9 +1,4 @@
-// function inputValue(idName){ 
-//         let input1 = document.getElementById(idName);
-//         let input1Value = input1.value;
-//         let input = parseFloat(input1Value);
-//         return input;
-// }
+
 function inputValue(idName){
 
         let input1 = document.getElementById(idName);
@@ -39,4 +34,15 @@ function getTableValue(name, total){
     <td><button class="bg-blue-500 rounded-lg text-white p-2 font-semibold">Convert to m<sup>2</sup></button></td>
     `;
     container.appendChild(tr);
+}
+
+var cards = document.getElementsByClassName('card');
+for (var i = 0; i < cards.length; i++) {
+    cards[i].onmouseover = function(e) {
+        var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+        var colorString = '0px 0px 30px 0px ' + color;
+        this.style['box-shadow'] = colorString;
+        this.style['-webkit-box-shadow'] = colorString;
+        this.style['-moz-box-shadow'] = colorString;
+    }  
 }
